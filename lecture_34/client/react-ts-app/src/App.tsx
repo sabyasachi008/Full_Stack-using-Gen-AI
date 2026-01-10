de9e28a54adfd2a4e7fc1628ffd58977e1b3b484
sabyasachi008 <123227102+sabyasachi008@users.noreply.github.com>
2026-01-11 00:32:52 +0530

    React Day - 1

---
+++ lecture_34/client/react-ts-app/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  console.log("We are console logging as well as updating");
+
+  const increaseAndLog= ()=> {
+    setCount(count+1);
+
+  };
+
+  return (
+
+    <>
+      <button onClick={increaseAndLog}>Click To increase</button>
+      <br></br>
+      <br></br><br></br>
+      <button onClick={()=> setCount(count-1)}>Click to decrease</button>
+      <p>{count}</p>
+    </>
+  );
+};
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Counter App</h1>
+    <p>This is my first React App which can count on clicks</p>
+    <Counter/>
+     
+    </>
+  )
+}
+
+export default App
